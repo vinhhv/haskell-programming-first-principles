f52b93342ba7db23ab113360171f62d0a1f3ad7f
Vinh Vu <kroomey@gmail.com>
2018-11-16 00:02:25 -0800

    Ch 26 MaybeT

---
+++ ch26_monad_transformers/MaybeT.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module MaybeT where
+
+newtype MaybeT m a = MaybeT { runMaybeT :: m (Maybe a) }
+
+instance Functor m => Functor (MaybeT m) where
+  fmap f (MaybeT ma) = MaybeT $ (fmap . fmap) f ma
+
+instance Applicative m => Applicative (MaybeT m) where
+  pure x = MaybeT $ pure (pure x)
+  MaybeT fab <*> MaybeT mma = MaybeT $ (<*>) <$> fab <*> mma
+
+instance Monad m => Monad (MaybeT m) where
+  return = pure
+
+  (>>=) :: MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
+  -- MaybeT mma >>= fab = MaybeT $ mma >>= runMaybeT . fab
+  MaybeT mma >>= f =
+    MaybeT $ do
+      v <- mma
+      case v of
+          Nothing -> return Nothing
+          Just y -> runMaybeT (f y)
