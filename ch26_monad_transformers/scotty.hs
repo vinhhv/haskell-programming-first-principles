99c888c8ead886b1cb6a67c3cc39b2c42b2a8010
Vinh Vu <kroomey@gmail.com>
2018-11-17 18:26:36 -0800

    Ch 26 scotty

---
+++ ch26_monad_transformers/scotty.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Scotty where
+
+import Web.Scotty
+import Web.Scotty.Internal.Types (ActionT(..))
+
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State.Lazy hiding (get)
+import Data.Monoid (mconcat)
+
+liftReaderT :: m a -> ReaderT r m a
+liftReaderT m = ReaderT $ const m
+
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    -- ActionT . lift . lift . lift $ putStrLn "hello"
+    -- lift $ putStrLn "hello"
+    -- liftM is the same as fmap
+    (ActionT
+     . (ExceptT . liftM Right)
+     . liftReaderT
+     . \m -> StateT (\s -> do
+                        a <- m
+                        return (a, s))
+     ) (putStrLn "hello")
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
